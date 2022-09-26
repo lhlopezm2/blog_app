3e096f77b27f271ef38ba8b70e548f1c28ab16ba
Luis <lhlopezm2@gmail.com>
2022-09-26 09:05:33 -0500

    Apply rubocop guidelines

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Controller for comments model
 class CommentsController < ApplicationController
-  #http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  # http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -14,7 +17,8 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
 end
